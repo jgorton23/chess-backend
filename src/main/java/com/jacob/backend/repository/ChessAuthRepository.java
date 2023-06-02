497653c4bfee10d9fd9540c611eedf5578cb789f
Jacob Gorton <jagorton@iastate.edu>
2023-06-02 14:11:37 -0500

    persistence context still not working

--- src/main/java/com/jacob/backend/repository/ChessAuthRepository.java
+++ src/main/java/com/jacob/backend/repository/ChessAuthRepository.java
@@ -1,17 +1,20 @@
 package com.jacob.backend.repository;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Repository;
 
 import com.jacob.backend.data.CredentialsDTO;
 import com.jacob.backend.data.User;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
 import jakarta.persistence.PersistenceContext;
 
 @Repository
 public class ChessAuthRepository implements ChessAuthRepositoryInterface {
 
-    @PersistenceContext
+    @PersistenceContext(unitName = "mypu")
     private EntityManager entityManager;
 
     public boolean login(CredentialsDTO cred) {
@@ -19,19 +22,21 @@ public class ChessAuthRepository implements ChessAuthRepositoryInterface {
     }
 
     public boolean register(User user) {
-        // entityManager.getTransaction().begin();
         entityManager.persist(user);
-        // entityManager.getTransaction().commit();
         return true;
     }
 
     public boolean userExists(String username) {
-
-        // User u = (User) entityManager.createQuery("SELECT user from users where user.username = ?1")
-        //         .setParameter(1, username)
-        //         .getSingleResult();
-        User u = entityManager.find(User.class, username);
+        // User u = entityManager.find(User.class, username);
+        User u = null;
+        try {
+            EntityManager em = Persistence.createEntityManagerFactory("mypu").createEntityManager();
+            u = em.find(User.class, UUID.randomUUID());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return u != null;
+
     }
 
     public String getUserHash(String username) {
