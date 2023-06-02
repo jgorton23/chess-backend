b8ed4fe01a557c70f63bad4acd14186ba1be87a2
Jacob Gorton <jagorton@iastate.edu>
2023-06-02 15:24:59 -0500

    persistence working

--- src/main/java/com/jacob/backend/repository/ChessAuthRepository.java
+++ src/main/java/com/jacob/backend/repository/ChessAuthRepository.java
@@ -1,40 +1,39 @@
 package com.jacob.backend.repository;
 
+import java.util.Objects;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.jacob.backend.data.CredentialsDTO;
 import com.jacob.backend.data.User;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @Repository
 public class ChessAuthRepository implements ChessAuthRepositoryInterface {
 
-    @PersistenceContext(unitName = "mypu")
+    @PersistenceContext
     private EntityManager entityManager;
 
     public boolean login(CredentialsDTO cred) {
         return true;
     }
 
+    @Transactional
     public boolean register(User user) {
         entityManager.persist(user);
         return true;
     }
 
     public boolean userExists(String username) {
-        // User u = entityManager.find(User.class, username);
-        User u = null;
-        try {
-            EntityManager em = Persistence.createEntityManagerFactory("mypu").createEntityManager();
-            u = em.find(User.class, UUID.randomUUID());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        User u;
+        u = entityManager.find(User.class, UUID.randomUUID());
         return u != null;
 
     }
