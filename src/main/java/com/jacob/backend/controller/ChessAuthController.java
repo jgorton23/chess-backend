9db1f75173330175db7128daed2a8ac664fd8053
Jacob Gorton <jagorton@iastate.edu>
2023-06-05 10:54:44 -0500

    better response for http

--- src/main/java/com/jacob/backend/controller/ChessAuthController.java
+++ src/main/java/com/jacob/backend/controller/ChessAuthController.java
@@ -1,6 +1,8 @@
 package com.jacob.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +19,32 @@ public class ChessAuthController {
     private ChessAuthService authService;
 
     @PostMapping("/login")
-    public String login(@RequestBody CredentialsDTO creds) {
-        return authService.login(creds);
+    public ResponseEntity<String> login(@RequestBody CredentialsDTO creds) {
+        String message;
+        HttpStatus status;
+        try {
+            message = authService.login(creds);
+            status = HttpStatus.OK;
+        } catch (Exception e) {
+            message = String.format("Error: $v", e);
+            status = HttpStatus.BAD_REQUEST;
+        }
+        ResponseEntity<String> r = new ResponseEntity<String>(message, null, status);
+        return r;
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody CredentialsDTO creds) {
-        return authService.register(creds);
+    public ResponseEntity<String> register(@RequestBody CredentialsDTO creds) {
+        String message;
+        HttpStatus status;
+        try {
+            message = authService.register(creds);
+            status = HttpStatus.OK;
+        } catch (Exception e) {
+            message = String.format("Error: $v", e);
+            status = HttpStatus.BAD_REQUEST;
+        }
+        ResponseEntity<String> r = new ResponseEntity<String>(message, null, status);
+        return r;
     }
 }
