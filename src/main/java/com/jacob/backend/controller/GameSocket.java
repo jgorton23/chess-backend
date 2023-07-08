d1485974f903c286a69518cd7bd1ab69de4cecb7
Jacob Gorton <jagorton@iastate.edu>
2023-07-08 10:56:09 -0500

    commented GameSocket

--- src/main/java/com/jacob/backend/controller/GameSocket.java
+++ src/main/java/com/jacob/backend/controller/GameSocket.java
@@ -10,19 +10,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jacob.backend.data.Model.Game;
 import com.jacob.backend.service.GameService;
 
+/**
+ * Controller containing endpoints for the WebSocket
+ */
 @RestController
 @CrossOrigin
 public class GameSocket {
 
+    /**
+     * Service containing Game related logic
+     */
     @Autowired
     private GameService gameService;
 
+    /**
+     * Service containing Message related logic
+     */
     @Autowired
     private SimpMessagingTemplate messaging;
 
+    /**
+     * Updates the Game in the database, and sends the updated gameState to the
+     * topoic
+     * 
+     * @param gameId the Id of the Game to update
+     * @param game   the new gameState to save and send
+     */
     @MessageMapping("/game/{gameId}")
     public void UpdateGame(@DestinationVariable String gameId, Game game) {
+
+        // send the game to the other users
         messaging.convertAndSend("/topic/game/" + gameId, game);
+
+        // persist the gameState in the db
         gameService.update(game);
+
     }
 }
