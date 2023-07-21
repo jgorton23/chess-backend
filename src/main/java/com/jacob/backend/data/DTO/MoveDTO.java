1e2157a4670b0a97aac3f69184b3b09660026b93
Jacob Gorton <jagorton@iastate.edu>
2023-07-20 21:03:35 -0500

    added comments to MoveDTO

--- src/main/java/com/jacob/backend/data/DTO/MoveDTO.java
+++ src/main/java/com/jacob/backend/data/DTO/MoveDTO.java
@@ -17,29 +17,65 @@ public class MoveDTO {
      */
     private String piece;
 
+    /**
+     * Creates a new MoveDTO object
+     */
     public MoveDTO() {
     }
 
+    /**
+     * Gets the starting square of this Move
+     * 
+     * @return an int array representing the x,y coordinates of the starting square
+     */
     public int[] getStartSquare() {
         return startSquare;
     }
 
+    /**
+     * Sets the starting square of this Move
+     * 
+     * @param newStartSquare an int array representing the new x,y coordinates of
+     *                       the starting square
+     */
     public void setStartSquare(int[] newStartSquare) {
         startSquare = newStartSquare;
     }
 
+    /**
+     * Gets the destination square of this Move
+     * 
+     * @return an int array representing the x,y coordinates of the destination
+     *         square
+     */
     public int[] getDestSquare() {
         return destSquare;
     }
 
+    /**
+     * Sets the destination square of this Move
+     * 
+     * @param newDestSquare an int array representing the new x,y coordinates of the
+     *                      destination square
+     */
     public void setDestSquare(int[] newDestSquare) {
         destSquare = newDestSquare;
     }
 
+    /**
+     * Gets the piece to move
+     * 
+     * @return a String representing the piece that will move
+     */
     public String getPiece() {
         return piece;
     }
 
+    /**
+     * Sets the piece to move
+     * 
+     * @param newPiece a String representing the new piece that will move
+     */
     public void setPiece(String newPiece) {
         piece = newPiece;
     }
