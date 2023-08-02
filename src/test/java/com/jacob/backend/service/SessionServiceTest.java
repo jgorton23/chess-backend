f70a52dd8ddfd385a205affb96d909a276d3a0a9
Jacob Gorton <jagorton@iastate.edu>
2023-08-01 20:08:59 -0500

    use tags to separate unit test and integration tests

---
+++ src/test/java/com/jacob/backend/service/SessionServiceTest.java
@@ -0,0 +1,13 @@
+package com.jacob.backend.service;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("UnitTest")
+public class SessionServiceTest {
+
+    @Test
+    public void isValidUUID_whenInvokedWithValidUUID_thenReturnsTrue() {
+
+    }
+}
