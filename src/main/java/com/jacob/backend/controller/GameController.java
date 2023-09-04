3be211d8790fee01afbe68c9aba7c1b785c0cc7d
Jacob Gorton <jagorton@iastate.edu>
2023-09-03 21:29:20 -0500

    comments and cleanup

--- src/main/java/com/jacob/backend/controller/GameController.java
+++ src/main/java/com/jacob/backend/controller/GameController.java
@@ -6,16 +6,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jacob.backend.data.DTO.MoveDTO;
 import com.jacob.backend.data.Model.*;
@@ -230,10 +221,13 @@ public class GameController {
 
         try {
 
+            // Get the username of the player, by session
             String username = sessionService.getUsernameById(sessionId);
 
+            // resign the user from the given game
             gameService.resign(username, gameId);
 
+            // Return success
             return ResponseEntity.ok().body(JSONResponses.success());
 
         } catch (UnauthorizedException e) {
