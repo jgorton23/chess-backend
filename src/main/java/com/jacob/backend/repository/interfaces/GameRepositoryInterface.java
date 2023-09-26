b11cc1a335c79bdcd1398abd8e1f7718fd8f4388
Jacob Gorton <jagorton@iastate.edu>
2023-09-25 21:17:30 -0500

    commentes the GameRepo interface

--- src/main/java/com/jacob/backend/repository/interfaces/GameRepositoryInterface.java
+++ src/main/java/com/jacob/backend/repository/interfaces/GameRepositoryInterface.java
@@ -7,11 +7,33 @@ import com.jacob.backend.data.Model.Game;
 
 public interface GameRepositoryInterface {
 
+    /**
+     * Gets the game with the given {@code UUID}
+     * 
+     * @param gameId the UUID of the game to get from the database
+     * @return the {@link Game} if it exists, otherwise null
+     */
     public Game getById(UUID gameId);
 
+    /**
+     * Gets all of the games for the user with the given {@code UUID}
+     * 
+     * @param userId the UUID of the user for whom to get all games from the db
+     * @return the list of {@link Game Games} for the user
+     */
     public List<Game> getAllByUserId(UUID userId);
 
+    /**
+     * Saves the given {@link Game} in the database
+     * 
+     * @param game the game to save
+     */
     public void save(Game game);
 
+    /**
+     * Updates the given {@link Game} in the database
+     * 
+     * @param game the game to update
+     */
     public void update(Game game);
 }
